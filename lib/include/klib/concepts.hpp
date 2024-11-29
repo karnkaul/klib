1e467903aaec7eaf572cf2986d5cb428c808bf72
Karn Kaul <karnkaul@gmail.com>
2024-11-29 15:52:29 -0800

    Put `concept ParamT` in `namespace args`

--- lib/include/klib/concepts.hpp
+++ lib/include/klib/concepts.hpp
@@ -10,8 +10,10 @@ template <typename Type>
 concept NumberT = !std::same_as<bool, Type> && (std::integral<Type> || std::floating_point<Type>);
 
 template <typename Type>
-concept ParamT = StringyT<Type> || NumberT<Type>;
-
-template <typename Type>
 concept NotBoolT = !std::same_as<Type, bool>;
+
+namespace args {
+template <typename Type>
+concept ParamT = StringyT<Type> || NumberT<Type>;
+} // namespace args
 } // namespace klib
