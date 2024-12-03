ad710b0d45d7572db5709e6eb22887626759f811
Karn Kaul <karnkaul@gmail.com>
2024-12-02 17:55:23 -0800

    Add `Polymorphic`, define `KLIB_DEBUG`.

--- lib/include/klib/constants.hpp
+++ lib/include/klib/constants.hpp
@@ -11,4 +11,11 @@ constexpr std::uint64_t kilo_v = 1000;
 constexpr std::uint64_t mega_v = 1000 * kilo_v;
 constexpr std::uint64_t giga_v = 1000 * mega_v;
 constexpr std::uint64_t tera_v = 1000 * giga_v;
+
+constexpr auto debug_v =
+#if defined(KLIB_DEBUG)
+	true;
+#else
+	false;
+#endif
 } // namespace klib
