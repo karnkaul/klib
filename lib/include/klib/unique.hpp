25dfda14c21978b065c85bc5cd21c89bc6c703e6
Karn Kaul <karnkaul@gmail.com>
2024-12-17 14:08:51 -0800

    Improve `Unique`, add `Noop`

--- lib/include/klib/unique.hpp
+++ lib/include/klib/unique.hpp
@@ -1,31 +1,64 @@
 #pragma once
 #include <klib/macros.hpp>
+#include <klib/noop.hpp>
+#include <concepts>
 #include <utility>
 
 namespace klib {
-template <typename Type, typename Deleter>
+template <typename Type>
+concept UniquePayloadT = std::is_default_constructible_v<Type> && std::move_constructible<Type>;
+
+template <UniquePayloadT Type>
+	requires std::equality_comparable<Type>
+struct UniqueIdentity {
+	constexpr auto operator()(Type const& t) const noexcept -> bool { return t == Type{}; }
+};
+
+template <UniquePayloadT Type, typename Deleter = Noop<Type>, typename Id = UniqueIdentity<Type>>
 class Unique {
   public:
+	using value_type = Type;
+	using deleter_type = Deleter;
+	using id_type = Id;
+
 	Unique(Unique const&) = delete;
 	auto operator=(Unique const&) -> Unique& = delete;
 
-	constexpr Unique(Type t = Type{}, Deleter deleter = Deleter{}) : m_t(std::move(t)), m_deleter(std::move(deleter)) {}
+	Unique() = default;
 
-	constexpr Unique(Unique&& rhs) noexcept : m_t(std::exchange(rhs.m_t, Type{})), m_deleter(std::move(rhs.m_deleter)) {}
+	constexpr Unique(Type t, Deleter deleter = Deleter{}, Id id = {}) : m_t(std::move(t)), m_deleter(std::move(deleter)), m_id(std::move(id)) {}
+
+	constexpr Unique(Unique&& rhs) noexcept : m_t(std::exchange(rhs.m_t, Type{})), m_deleter(std::move(rhs.m_deleter)), m_id(std::move(rhs.m_id)) {}
 
 	constexpr auto operator=(Unique&& rhs) noexcept -> Unique& {
 		using std::swap;
 		if (this != &rhs) {
 			swap(m_t, rhs.m_t);
 			swap(m_deleter, rhs.m_deleter);
+			swap(m_id, rhs.m_id);
 		}
 		return *this;
 	}
 
-	constexpr ~Unique() { m_deleter(std::move(m_t)); }
+	constexpr ~Unique() {
+		if (is_identity()) { return; }
+		m_deleter(std::move(m_t));
+	}
+
+	[[nodiscard]] constexpr auto is_identity() const -> bool { return m_id(m_t); }
+
+	[[nodiscard]] constexpr auto get() const -> Type const& { return m_t; }
+	[[nodiscard]] constexpr auto get() -> Type& { return m_t; }
+
+	[[nodiscard]] constexpr auto get_deleter() const -> deleter_type const& { return m_deleter; }
+	[[nodiscard]] constexpr auto get_id() const -> id_type const& { return m_id; }
+
+	constexpr operator Type const&() const { return get(); }
+	constexpr operator Type&() const { return get(); }
 
   private:
 	Type m_t;
 	KLIB_NO_UNIQUE_ADDRESS Deleter m_deleter;
+	KLIB_NO_UNIQUE_ADDRESS Id m_id;
 };
 } // namespace klib
---
+++ tests/test_unique.cpp
@@ -0,0 +1,51 @@
+#include <klib/unique.hpp>
+
+namespace {
+using namespace klib;
+
+consteval auto unique_int() {
+	auto i = Unique<int>{};
+	i = 42;
+	auto j = std::move(i);
+	if (!i.is_identity()) { return false; }
+	if (i.get() != 0) { return false; }
+	return j.get() == 42;
+}
+static_assert(unique_int());
+
+consteval auto custom_id() {
+	struct Id {
+		consteval auto operator()(int const i) const { return i == -1; }
+	};
+	auto i = Unique<int, Noop<int>, Id>{-1};
+	if (!i.is_identity()) { return false; }
+	i = 42;
+	if (i.is_identity()) { return false; }
+	return i.get() == 42;
+}
+static_assert(custom_id());
+
+consteval auto custom_deleter() {
+	auto deleted = int{};
+	auto called = int{};
+
+	{
+		struct Deleter {
+			int* deleted{};
+			int* called{};
+			consteval void operator()(int const i) const {
+				if (deleted == nullptr || called == nullptr) { return; }
+				*deleted = i;
+				++(*called);
+			}
+		};
+		auto i = Unique<int, Deleter>{1, {.deleted = &deleted, .called = &called}};
+		if (deleted != 0 || called != 0) { return false; }
+		i = {42, {.deleted = &deleted, .called = &called}};
+		if (deleted != 1 || called != 1) { return false; }
+	}
+
+	return called == 2 && deleted == 42;
+}
+static_assert(custom_deleter());
+} // namespace
