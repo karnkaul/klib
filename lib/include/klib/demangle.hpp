c06d85ec4f2492323cba76ea9507fa4ff537d6c3
Karn Kaul <karnkaul@gmail.com>
2025-09-17 13:39:37 -0700

    Dynamic demangle for polymorphic types

--- lib/include/klib/demangle.hpp
+++ lib/include/klib/demangle.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <klib/concepts.hpp>
 #include <string>
 #include <typeinfo>
 
@@ -9,5 +10,10 @@ template <typename Type>
 [[nodiscard]] auto demangled_name() -> std::string const& {
 	static std::string const ret = [] { return demangled_name(typeid(Type)); }();
 	return ret;
+}
+
+template <PolymorphicT Type>
+[[nodiscard]] auto demangled_name(Type const& t) -> std::string {
+	return demangled_name(typeid(t));
 }
 } // namespace klib
--- tests/test_demangle.cpp
+++ tests/test_demangle.cpp
@@ -2,9 +2,23 @@
 #include <klib/demangle.hpp>
 #include <klib/unit_test.hpp>
 
+namespace test {
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
+struct Base {
+	virtual ~Base() = default;
+};
+
+struct Derived : Base {};
+} // namespace test
+
 namespace {
 TEST(demangle) {
-	auto const& name = klib::demangled_name<klib::CString>();
+	auto name = klib::demangled_name<klib::CString>();
 	EXPECT(name == "klib::CString");
+
+	auto const derived = test::Derived{};
+	test::Base const& base = derived;
+	name = klib::demangled_name(base);
+	EXPECT(name == "test::Derived");
 }
 } // namespace
