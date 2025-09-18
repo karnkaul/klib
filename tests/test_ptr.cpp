b82612a59f668602416ef69e30b88c19bace6526
Karn Kaul <karnkaul@gmail.com>
2025-09-18 15:28:35 -0700

    `Ptr<T>`

---
+++ tests/test_ptr.cpp
@@ -0,0 +1,19 @@
+#include <klib/ptr.hpp>
+
+namespace {
+template <typename T>
+using Ptr = klib::Ptr<T>;
+
+struct A {};
+struct B : A {};
+
+constexpr auto a = A{};
+constexpr auto p_a = Ptr{&a};
+static_assert(p_a);
+static_assert(p_a == &a);
+
+constexpr auto b = B{};
+constexpr Ptr<A const> p_b{&b};
+static_assert(p_b);
+static_assert(p_b == Ptr{&b});
+} // namespace
