2598bccad2e6480695402db27498ece542c1fec0
Karn Kaul <karnkaul@gmail.com>
2025-09-26 18:24:51 -0700

    Pass CMake args to install script

--- install_test/script.sh
+++ install_test/script.sh
@@ -12,7 +12,7 @@ cmake --install ../$build_dir --config=Debug --prefix=./packages || exit 1
 cmake --install ../$build_dir --config=Release --prefix=./packages || exit 1
 
 cp -f ../CMakePresets.json ./exe
-cmake -S exe --preset=default -B=build -DCMAKE_INSTALL_PREFIX=./packages || exit 1
+cmake -S exe --preset=default -B=build -DCMAKE_INSTALL_PREFIX=./packages ${@:2} || exit 1
 
 cmake --build build --config=Debug || exit 1
 build/Debug/exe || exit 1
