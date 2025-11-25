916c4e68c85884a836055cc94329988d6c2943fc
Karn Kaul <karnkaul@gmail.com>
2025-11-25 08:58:02 -0800

    Patch: improve `CString`

--- lib/include/klib/c_string.hpp
+++ lib/include/klib/c_string.hpp
@@ -1,17 +1,31 @@
 #pragma once
-#include <string_view>
+#include <string>
 
 namespace klib {
 /// \brief Wrapper over a C string.
 /// Never points to nullptr.
 class CString {
   public:
-	explicit(false) constexpr CString(char const* str = "") : m_str(str == nullptr ? "" : str) {}
+	CString() = default;
 
-	[[nodiscard]] constexpr auto c_str() const -> char const* { return as_view().data(); }
+	CString(std::nullptr_t) = delete;
+
+	template <std::size_t N>
+	// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+	explicit(false) constexpr CString(char const (&str)[N]) : m_str(str, N) {}
+
+	explicit(false) constexpr CString(char const* str) : m_str(str ? str : "") {}
+
+	template <std::same_as<std::string> T>
+	explicit(false) constexpr CString(T const& str) : m_str(str) {}
+
 	[[nodiscard]] constexpr auto as_view() const -> std::string_view { return m_str; }
+	[[nodiscard]] constexpr auto c_str() const -> char const* { return m_str.data(); }
+
+	auto operator<=>(CString const&) const = default;
 
   private:
-	std::string_view m_str;
+	// NOLINTNEXTLINE(readability-redundant-string-init)
+	std::string_view m_str{""};
 };
 } // namespace klib
