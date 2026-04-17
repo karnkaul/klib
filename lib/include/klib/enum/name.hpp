8c03bba35ab70ea905629e47322b32b21a63aed4
Karn Kaul <karnkaul@gmail.com>
2026-04-16 22:02:23 -0700

    [v0.6.4] Patch: use `std::pair` for `EnumNameMap` (#48)

--- lib/include/klib/enum/name.hpp
+++ lib/include/klib/enum/name.hpp
@@ -3,33 +3,28 @@
 #include <algorithm>
 #include <optional>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 namespace klib {
-template <EnumT E>
-struct EnumNameMapping {
-	E e{};
-	std::string_view name{};
-};
-
 template <EnumT E, template <typename...> typename ContainerT = std::vector, typename... Args>
 class EnumNameMap {
   public:
-	using Entry = EnumNameMapping<E>;
+	using Entry = std::pair<E, std::string_view>;
 
 	EnumNameMap() = default;
 
 	explicit constexpr EnumNameMap(std::initializer_list<Entry> const& entries) : m_entries(entries) {}
 
 	[[nodiscard]] constexpr auto to_name(E const e) const -> std::string_view {
-		auto const pred = [e](Entry const& entry) { return entry.e == e; };
-		if (auto const it = std::ranges::find_if(m_entries, pred); it != m_entries.end()) { return it->name; }
+		auto const pred = [e](Entry const& entry) { return entry.first == e; };
+		if (auto const it = std::ranges::find_if(m_entries, pred); it != m_entries.end()) { return it->second; }
 		return {};
 	}
 
 	[[nodiscard]] constexpr auto to_enum(std::string_view const name) const -> std::optional<E> {
-		auto const pred = [name](Entry const& entry) { return entry.name == name; };
-		if (auto const it = std::ranges::find_if(m_entries, pred); it != m_entries.end()) { return it->e; }
+		auto const pred = [name](Entry const& entry) { return entry.second == name; };
+		if (auto const it = std::ranges::find_if(m_entries, pred); it != m_entries.end()) { return it->first; }
 		return {};
 	}
 
